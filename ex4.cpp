16395b5aa16f9e3c2a02df819049baf95e2807e4
AndreiTrif88 <101469868+AndreiTrif88@users.noreply.github.com>
2022-03-12 19:15:22 +0200

    Create ex4.cpp

---
+++ ex4.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[100],b[100],n,m,c[100],p=0,i,j;
+    cin>>n;
+    cin>>m;
+    for(i=1;i<=n;i++)
+        cin>>a[i];
+        
+    for(j=1;j<=m;j++)
+        cin>>b[j];
+    i=1;
+    j=1;
+    while (i<=n || j<=m)
+        if (a[i]==b[j])
+        {
+            p++;
+            c[p]=a[i];
+            i++;
+            j++;
+        }
+        else
+            if (a[i]>a[j])
+                j++;
+            else
+                i++;
+    for (i=1;i<=p;i++)
+        cout <<c[i]<< " ";
+}
