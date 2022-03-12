b1ad6fcc15cb0c1b5a488ad270ebc510a851c16a
AndreiTrif88 <101469868+AndreiTrif88@users.noreply.github.com>
2022-03-12 19:05:23 +0200

    Create ex2.cpp

---
+++ ex2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a[100],b[100],n,m,c[100],p=0,i,j;
+    cin>>n;
+    cin>>m;
+    for(i=1;i<=n;i++)
+        cin>>a[i];
+        
+    for(j=1;j<=m;j++)
+        cin>>b[j];
+    i=n;
+    j=m;
+    while (i>=1 && j>=1)
+        if (a[i]>b[j])
+        {
+            p++;
+            c[p]=a[i];
+            i--;
+        }
+        else
+        {
+            p++;
+            c[p]=b[j];
+            j--;
+        }
+    for (i=1;i<=p;i++)
+        cout <<c[i]<< " ";
+}
